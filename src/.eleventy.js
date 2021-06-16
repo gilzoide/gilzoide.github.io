a017fcda37f6a054588b8d82fa2c58313fa8de98
gilzoide <gilzoide@gmail.com>
2021-06-16 15:33:44 -0300

    Add About page with picture, minify HTML in build

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,3 +1,7 @@
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier")
+
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addPlugin(eleventyPluginFilesMinifier)
+  eleventyConfig.addPassthroughCopy("src/css")
+  eleventyConfig.addPassthroughCopy("src/images")
 };
