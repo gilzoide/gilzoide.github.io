c51f3b2919097af7bfc713d8e49790275abf609b
gilzoide <gilzoide@gmail.com>
2022-04-24 15:58:10 -0300

    Update eleventy, add markdown-it-anchor

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,10 +1,18 @@
 const beautify_html = require('js-beautify').html
+const mdIt = require('markdown-it')
+const mdItAnchor = require('markdown-it-anchor')
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addNunjucksFilter("datestr", function(date) {
-    return date.toISOString().split('T')[0]
+function setupMarkdownIt(eleventyConfig) {
+  let md = mdIt({
+    html: true,
+    linkify: true,
+  }).use(mdItAnchor, {
+    permalink: mdItAnchor.permalink.headerLink(),
   })
+  eleventyConfig.setLibrary("md", md)
+}
 
+function setupHtmlBeautifier(eleventyConfig) {
   eleventyConfig.addTransform("processHTML", function(content, outputPath) {
     if (outputPath && outputPath.endsWith(".html")) {
       return beautify_html(content, {
@@ -14,7 +22,26 @@ module.exports = function(eleventyConfig) {
     }
     return content
   })
+}
+
+function setupNunjucksFilters(eleventyConfig) {
+  eleventyConfig.addNunjucksFilter("datestr", function(date) {
+    return date.toISOString().split('T')[0]
+  })
+}
+
+function setupPassthroughFolders(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/lib")
   eleventyConfig.addPassthroughCopy("src/css")
   eleventyConfig.addPassthroughCopy("src/images")
-};
+}
+
+module.exports = function(eleventyConfig) {
+  setupNunjucksFilters(eleventyConfig)
+
+  setupMarkdownIt(eleventyConfig)
+
+  setupHtmlBeautifier(eleventyConfig)
+
+  setupPassthroughFolders(eleventyConfig)
+}
